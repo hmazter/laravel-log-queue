ebd6017504da345ed22ddff5b55a8246142e90ec
Kristoffer Högberg <krihog@gmail.com>
2026-09-10 20:34:31 +0200

    Add Laravel 13 queue contract methods to LogQueue

--- src/LogQueue.php
+++ src/LogQueue.php
@@ -81,4 +81,48 @@ class LogQueue extends Queue implements QueueContract
     {
         return 0;
     }
+
+    /**
+     * Get the number of pending jobs.
+     *
+     * @param  string|null $queue
+     * @return int
+     */
+    public function pendingSize($queue = null)
+    {
+        return 0;
+    }
+
+    /**
+     * Get the number of delayed jobs.
+     *
+     * @param  string|null $queue
+     * @return int
+     */
+    public function delayedSize($queue = null)
+    {
+        return 0;
+    }
+
+    /**
+     * Get the number of reserved jobs.
+     *
+     * @param  string|null $queue
+     * @return int
+     */
+    public function reservedSize($queue = null)
+    {
+        return 0;
+    }
+
+    /**
+     * Get the creation timestamp of the oldest pending job, excluding delayed jobs.
+     *
+     * @param  string|null $queue
+     * @return int|null
+     */
+    public function creationTimeOfOldestPendingJob($queue = null)
+    {
+        return null;
+    }
 }
